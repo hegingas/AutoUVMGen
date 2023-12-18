e348b221abdd8b8d6806f19bb5848d18a2e80066
ginga <1120640784@qq.com>
2023-12-18 23:33:55 +0800

    更新创建窗口文件，当前未实现任何功能

---
+++ python/createWindow.py
@@ -0,0 +1,20 @@
+import sys
+from PySide6.QtWidgets import *
+
+myApp = QApplication(sys.argv)
+
+myWindow = QWidget()
+myWindow.setWindowTitle('UVM Automatic Tool')
+myWindow.resize(300,150)
+
+myLabel = QLabel(myWindow)
+myLabel.setText('输入信息')
+myLabel.setGeometry(80,50,150,20)
+
+myButton = QPushButton(myWindow)
+myButton.setText("开始")
+myButton.setGeometry(120,100,50,20)
+
+myWindow.show()
+
+sys.exit(myApp.exec())
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
 export PRJ_BASE=`pwd`
+export SIM_BASE=$PRJ_BASE/sim
